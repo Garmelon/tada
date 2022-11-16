4aecce8107d01d4a946759d5ffd0625a9e45961f
Joscha <joscha@plugh.de>
2022-11-16 23:56:58 +0100

    Parse command line args

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,36 @@
+use std::fs;
+use std::path::PathBuf;
+
+use clap::Parser;
+
 mod builtin;
 mod table;
 mod value;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Parser)]
+enum Command {
+    Parse { file: PathBuf },
+}
+
+#[derive(Parser)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+/// Foo
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    match args.command {
+        Command::Parse { file } => {
+            let content = fs::read_to_string(&file)?;
+            print!("{content}");
+            if !content.ends_with('\n') {
+                println!();
+            }
+        }
+    }
+
+    Ok(())
 }
