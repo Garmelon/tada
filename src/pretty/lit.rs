f91e8ac9a2d5f0ec89d7ce070452593847f5f187
Joscha <joscha@plugh.de>
2022-11-20 23:06:04 +0100

    Pretty print string literals

--- src/pretty/lit.rs
+++ src/pretty/lit.rs
@@ -1,10 +1,32 @@
 use pretty::{DocAllocator, DocBuilder, Pretty};
 
-use crate::ast::{Lit, NumLit};
+use crate::ast::{Lit, NumLit, StringLit, StringLitElem};
 
 impl<'a, D: DocAllocator<'a>> Pretty<'a, D> for NumLit {
     fn pretty(self, allocator: &'a D) -> DocBuilder<'a, D> {
         allocator.text(format!("{self:?}"))
+    }
+}
+
+impl<'a, D: DocAllocator<'a>> Pretty<'a, D> for StringLitElem {
+    fn pretty(self, allocator: &'a D) -> DocBuilder<'a, D> {
+        match self {
+            Self::Plain(str) => allocator.text(str),
+            Self::Unicode(char) => allocator.text(format!("\\u{{{:x}}}", char as u32)),
+            Self::Backslash => allocator.text("\\\\"),
+            Self::DoubleQuote => allocator.text("\\\""),
+            Self::Tab => allocator.text("\\t"),
+            Self::CarriageReturn => allocator.text("\\r"),
+            Self::Newline => allocator.text("\\n"),
+        }
+    }
+}
+
+impl<'a, D: DocAllocator<'a>> Pretty<'a, D> for StringLit {
+    fn pretty(self, allocator: &'a D) -> DocBuilder<'a, D> {
+        allocator
+            .concat(self.elems.into_iter().map(|e| e.pretty(allocator)))
+            .enclose(allocator.text("\""), allocator.text("\""))
     }
 }
 
@@ -16,7 +38,7 @@ impl<'a, D: DocAllocator<'a>> Pretty<'a, D> for Lit {
             Self::Bool(true, _) => allocator.text("true"),
             Self::Builtin(builtin, _) => allocator.text(format!("{builtin:?}")),
             Self::Num(num) => num.pretty(allocator),
-            Self::String(string) => allocator.text("<string>"),
+            Self::String(string) => string.pretty(allocator),
             Self::Table(table) => allocator.text("<table>"),
         }
     }
