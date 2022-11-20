3ed3c4e8f884dbb8b84021485cc7f8968871cda2
Joscha <joscha@plugh.de>
2022-11-20 22:15:43 +0100

    Add warnings from cove

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+#![deny(unsafe_code)]
+// Rustc lint groups
+#![warn(future_incompatible)]
+#![warn(rust_2018_idioms)]
+// Rustc lints
+#![warn(noop_method_call)]
+#![warn(single_use_lifetimes)]
+#![warn(trivial_numeric_casts)]
+#![warn(unused_crate_dependencies)]
+#![warn(unused_extern_crates)]
+#![warn(unused_import_braces)]
+#![warn(unused_lifetimes)]
+#![warn(unused_qualifications)]
+// Clippy lints
+#![warn(clippy::use_self)]
+
 use std::fs;
 use std::path::PathBuf;
 
